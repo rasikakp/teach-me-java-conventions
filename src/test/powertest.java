386c18707a72c58bf1b1421a4dd320af63bc7944
rasikakp <99480188+rasikakp@users.noreply.github.com>
2022-02-12 13:22:02 +0530

    Fixed ClassName, functionNames and white spaces.

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -1,26 +1,25 @@
 package powerpackage;
 
-
 import org.junit.Test;
 
-public class powertest {
+public class PowerTest {
     @Test
-    public void one_raised_to_one_is_onew() {
+    public void oneRaisedToOneIsOnew() {
         assert powerfinder.OF(1,1) == 1;
     }
 
     @Test
-    public void twoRaised_to_one_is_2() {
+    public void twoRaisedToOneIs2() {
         assert powerfinder.OF(2,1) == 2;
     }
 
     @Test
-    public void two_power_2_is_4() {
+    public void twoPower2Is4() {
         assert powerfinder.OF(2, 2) == 4;
     }
 
     @Test
-    public void power_of_2_and_3_Is_SIX() {
+    public void powerOf2And3IsSIX() {
         assert powerfinder.OF(3, 2) == 3*3;
     }
 }
